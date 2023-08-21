be0df6656f4acbe343e1cfedfa6443a7cd790146
ShihabIbrahem74 <shihabibrahem4@gmail.com>
2023-08-21 22:01:17 +0300

    fixing conflict

--- _printf.c
+++ _printf.c
@@ -19,40 +19,17 @@ int _printf(const char *format, ...)
 		if (format[string_counter] == '%')
 		{
 			string_counter++;
-<<<<<<< HEAD
-			switch (format[string_counter])
-			{
-				case 'c':
-				_putchar(va_arg(arguments, int));
-				printed_counter++;
-				break;
-				case 's':
-				string_buffer = va_arg(arguments, char *);
-				_puts(string_buffer);
-				printed_counter += _strlen(string_buffer);
-				break;
-				case '%':
-				_putchar('%');
-				printed_counter++;
-				break;
-				case 'd':
-
-				default:
-				return (-1);
-			}
-=======
 			if (format[string_counter] == 'c')
 				printed_counter = print_character(arguments, printed_counter);
 			else if (format[string_counter] == 's')
 				printed_counter = print_string(arguments, printed_counter);
 			else if (format[string_counter] == '%')
 				printed_counter = print_percent(printed_counter);
->>>>>>> 629921611788505f3d96732f181b25ac23ce513d
 		}
 		else
 		{
-		    _putchar(format[string_counter]);
-		    printed_counter++;
+			_putchar(format[string_counter]);
+			printed_counter++;
 		}
 		string_counter++;
 	}
