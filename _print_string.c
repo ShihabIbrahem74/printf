82accbdf53e2fb674b706a768932609b73557f4e
ShihabIbrahem74 <shihabibrahem4@gmail.com>
2023-08-21 04:59:39 +0300

    uploading _puts and _putchar functions

---
+++ _print_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _putchar - print character
+ * Description: print a single character and send number of bytes written
+ * @c: a signle char
+ * Return: 1 on sucess
+*/
+
+int _putchar(char c)
+{
+	unsigned int i;
+
+	i = write(1, &c, 1);
+	return (i);
+}
+
+/**
+ * _puts - print a complete string
+ * Description: it send every char to _putchar to print
+ * @str: '*str' is a pointer pointing the first char in the string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	unsigned int counter_string = 0;
+
+	while (str[counter_string] != 0)
+	{
+		_putchar(str[counter_string]);
+		counter_string++;
+	}
+}
