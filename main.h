308996e0c38faac543d199f70ec34675f37ab8bb
ShihabIbrahem74 <shihabibrahem4@gmail.com>
2023-08-21 04:58:15 +0300

    uploading header file

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* LIBRARIES */
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+
+/* FUNCTIONS */
+int _printf(const char *format, ...);
+int _putchar(char c);
+void _puts(char *str);
+int _strlen(char *s);
+
+#endif
