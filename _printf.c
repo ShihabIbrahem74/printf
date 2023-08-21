0fdc07675fe05e1b935cb55112965cc97d8fb8ae
ShihabIbrahem74 <shihabibrahem4@gmail.com>
2023-08-21 05:19:30 +0300

    Fixing main function

--- _printf.c
+++ _printf.c
@@ -23,7 +23,7 @@ int _printf(const char *format, ...)
 			switch (format[string_counter])
 			{
 				case 'c':
-				_putchar(va_arg(arguments, int));
+				printf("%c", va_arg(arguments, int));
 				printed_counter++;
 				break;
 				case 's':
