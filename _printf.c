b6131aa2663bcc5144dd4a362009e90b6e57c397
zezomo <zezouniverse7@gmail.com>
2023-08-21 06:19:23 -0700

    from zezo

--- _printf.c
+++ _printf.c
@@ -14,9 +14,9 @@ int _printf(const char *format, ...)
 
 	va_start(arguments, format);
 	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
+	return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
+	return (-1);
 	while (format[string_counter])
 	{
 		if (format[string_counter] == '%')
