84619ebf3ce0bb8de2e2cd6df7037dc438d1bdb6
zezomo <zezouniverse7@gmail.com>
2023-08-21 20:43:35 -0700

    from zezo

--- main.h
+++ main.h
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <limits.h>
+#include <math.h>
 
 
 
@@ -16,11 +17,13 @@ int _putchar(char c);
 void _puts(char *str);
 int _strlen(char *s);
 int _check_strings(const char *chk_format, ...);
+int convert_binery(int dec);
 
 /* PRINT FUNCTIONS */
 int print_character(va_list arguments, int printed_counter);
 int print_string(va_list arguments, int printed_counter);
 int print_percent(int printed_counter);
 int print_integer(va_list arguments, int printed_counter);
+int print_binary(va_list arguments, int printed_counter);
 
 #endif
