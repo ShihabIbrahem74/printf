01e1d933935ab66ceecc10963cfd1de362c4e554
zezomo <zezouniverse7@gmail.com>
2023-08-22 18:33:50 -0700

    from zezo

--- revers.c
+++ revers.c
@@ -1,27 +1,5 @@
 #include "main.h"
 /**
- * rev_string - check the codefawdf fa fawfdw fa fwf f fwaffawf
- *
- * @s: adfwfw afgawf wadfa f wfaf wfdaw wd dawd eaegesgegeg
- *
- * Return: Always 0.
- */
-char *rev_string(char *c)
-{
-    int i, len;
-    char temp;
-    len = _strlen(c);
-    
-    for (i = 0; i < len / 2; i++)
-    {
-        temp = c[i];
-        c[i] = c[len - i - 1];
-        c[len - i - 1] = temp;
-    }
-    return (c);
-    
-}
-/**
  * reverse_string - helafsfaf fa fa ffafafaf f afar awe faf aef afaf af fafaf
  *
  * @arguments: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
@@ -32,13 +10,17 @@ char *rev_string(char *c)
  */
 int reverse_string(va_list arguments, int printed_counter)
 {
-        char *string_revers;
         char *string_buffer;
-
+        int len;
         string_buffer = va_arg(arguments, char *);
-        string_revers = rev_string(string_buffer);
-        _puts(string_revers);
-        printed_counter += _strlen(string_revers);
-        return (printed_counter);
+        len = _strlen(string_buffer);
+        len--;
+        printed_counter+= len;
+        while (len != -1)
+        {
+            _putchar(string_buffer[len]);
+            len--;
+	}
+	return (printed_counter);
 
 }
